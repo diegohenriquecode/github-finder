43ad9e92b87e6c865aafc346f38243312ae18e1e
diegoholiveirav <diegoholiveirav@gmail.com>
2021-03-24 00:49:17 -0300

    sleep time - ultilizando useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from "react";
+
 import { RepositoryItem } from "./RepositoryItem";
+import "../styles/repositories.scss";
+
+// https://api.github.com/orgs/groove.tech/repos
 
 const repository = {
   name: "Dieguinho",
@@ -7,6 +12,14 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios!</h1>
