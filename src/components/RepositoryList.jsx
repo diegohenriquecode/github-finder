cd4ad13ffdc7ed7c9c8730a3b6019c873498917f
diegoholiveirav <diegoholiveirav@gmail.com>
2021-03-24 22:12:28 -0300

    listando os repositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,14 +3,6 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 
-// https://api.github.com/orgs/groove.tech/repos
-
-const repository = {
-  name: "Dieguinho",
-  description: "Essa fera meu!",
-  link: "https://github.com/diegoholiveirav",
-};
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -24,9 +16,9 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de Repositórios!</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
